6337b9e49652ebf7ae18ee150aea1bd73d14b849
ctrlsumit <palsumitkumar2004@gmail.com>
2024-07-06 00:05:16 +0530

    commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,6 +7,8 @@ export const generateToken = (user, message, statusCode, res) => {
   .cookie(cookieName, token, {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE *24*60*60*1000), // Expires in 30 days
     httpOnly: true,
+    secure :true,
+    sameSite:"None"
   })
   .json({
     success: true,
